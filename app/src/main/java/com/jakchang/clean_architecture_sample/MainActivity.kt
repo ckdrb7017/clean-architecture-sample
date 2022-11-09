dd694d907b91ea1c4aa3ae288cb161c958164b1d
changgyu <ckdrb7017@hanmail.net>
2022-11-09 23:58:09 +0900

    [Update] Application 추가 및 적용

--- app/src/main/java/com/jakchang/clean_architecture_sample/MainActivity.kt
+++ app/src/main/java/com/jakchang/clean_architecture_sample/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.jakchang.clean_architecture_sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.jakchang.clean_architecture_sample.R
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
